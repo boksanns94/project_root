1ae9d7677811e374edf7b00dbdd27413bcfab44d
Milos Boksan <bigboks1@gmail.com>
2017-07-23 21:20:29 +0200

    Updates to initial state of the entire project. Renamed README.txt to README. Removed <UML Diagram> from project

---
+++ backEnd_root/backEnd_root/src/main/java/com/milosboksan/backendroot/BackEndRootApplication.java
@@ -0,0 +1,12 @@
+package com.milosboksan.backendroot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackEndRootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackEndRootApplication.class, args);
+	}
+}
---
+++ backEnd_root/backEnd_root/src/test/java/com/milosboksan/backendroot/BackEndRootApplicationTests.java
@@ -0,0 +1,16 @@
+package com.milosboksan.backendroot;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BackEndRootApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
